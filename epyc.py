166af9e73c39b779277d9a8a7c72b5685039bbb9
cyphar <cyphar@cyphar.com>
2014-01-11 07:47:10 +1100

    epyc: changed test wrapper to be more usable

--- epyc.py
+++ epyc.py
@@ -2,17 +2,23 @@
 
 import tokeniser
 import parser
-#import render
 
 def epyc(content, scope={}):
-  return parser.Parser(tokeniser.tokenise(content)).parse().render(scope)
+	return parser.Parser(tokeniser.tokenise(content)).parse().render(scope)
 
 if __name__ == "__main__":
-  import render
-  string = "\n".join(iter(input, ""))
-  print(epyc(string, {}))
+	import sys
+
+	if len(sys.argv) > 1:
+		with open(sys.argv[1]) as f:
+			string = f.read()
+	else:
+		string = "\n".join(iter(input, ""))
+
+	print(epyc(string, {}))
 
 # Node Testing
-# print(ForNode("a", "[1, 2, 3]", GroupNode([TextNode("hello, a:"), ExprNode("a"), TextNode("!\n")])).render({}))
-# print(IfNode("x > 10", TextNode("Statement is TRUE"), TextNode("Statement is FALSE")).render({'x':5}))
-# print(render.IncludeNode("test.py").render({}))
+	#import render
+	#print(render.ForNode("a", "[1, 2, 3]", render.GroupNode([render.TextNode("hello, a:"), render.ExprNode("a"), render.TextNode("!\n")])).render({}))
+	#print(render.IfNode("x > 10", render.TextNode("Statement is TRUE"), render.TextNode("Statement is FALSE")).render({'x':5}))
+	#print(render.IncludeNode("test.py").render({}))
