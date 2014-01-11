cde4140e82f15d9f0aca31f515bf7fd22ea0057d
cyphar <cyphar@cyphar.com>
2014-01-11 14:58:11 +1100

    epyc: update tests

--- epyc.py
+++ epyc.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
 	else:
 		string = "\n".join(iter(input, ""))
 
-	print(epyc(string, {}))
+	print(_render(string, {"a": [1, 2, 3]}))
 
 # Node Testing
 	#import render
 	#print(render.ForNode("a", "[1, 2, 3]", render.GroupNode([render.TextNode("hello, a:"), render.ExprNode("a"), render.TextNode("!\n")])).render({}))
 	#print(render.IfNode("x > 10", render.TextNode("Statement is TRUE"), render.TextNode("Statement is FALSE")).render({'x':5}))
-	#print(render.IncludeNode("test.py").render({}))
+	#print(render.ForNode("a", "range(100)", render.GroupNode([render.IfNode("a % 3 == 0", render.TextNode("Fizz"), render.TextNode("Buzz"))])).render())
