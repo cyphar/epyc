66edd00eb37fbbf8db0c1cd1dc8769d5d467dc46
cyphar <cyphar@cyphar.com>
2014-01-11 15:50:35 +1100

    *: conform to group 4-space style

--- epyc.py
+++ epyc.py
@@ -4,26 +4,26 @@ import tokeniser
 import parser
 
 def render(fname, scope={}):
-	with open(fname) as f:
-		content = f.read()
-	return _render(content, scope)
+    with open(fname) as f:
+        content = f.read()
+    return _render(content, scope)
 
 def _render(content, scope={}):
-	return parser.Parser(tokeniser.tokenise(content)).parse().render(scope)
+    return parser.Parser(tokeniser.tokenise(content)).parse().render(scope)
 
 if __name__ == "__main__":
-	import sys
+    import sys
 
-	if len(sys.argv) > 1:
-		with open(sys.argv[1]) as f:
-			string = f.read()
-	else:
-		string = "\n".join(iter(input, ""))
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as f:
+            string = f.read()
+    else:
+        string = "\n".join(iter(input, ""))
 
-	print(_render(string, {"a": [1, 2, 3]}))
+    print(_render(string, {"a": [1, 2, 3]}))
 
 # Node Testing
-	#import render
-	#print(render.ForNode("a", "[1, 2, 3]", render.GroupNode([render.TextNode("hello, a:"), render.ExprNode("a"), render.TextNode("!\n")])).render({}))
-	#print(render.IfNode("x > 10", render.TextNode("Statement is TRUE"), render.TextNode("Statement is FALSE")).render({'x':5}))
-	#print(render.ForNode("a", "range(100)", render.GroupNode([render.IfNode("a % 3 == 0", render.TextNode("Fizz"), render.TextNode("Buzz"))])).render())
+    #import render
+    #print(render.ForNode("a", "[1, 2, 3]", render.GroupNode([render.TextNode("hello, a:"), render.ExprNode("a"), render.TextNode("!\n")])).render({}))
+    #print(render.IfNode("x > 10", render.TextNode("Statement is TRUE"), render.TextNode("Statement is FALSE")).render({'x':5}))
+    #print(render.ForNode("a", "range(100)", render.GroupNode([render.IfNode("a % 3 == 0", render.TextNode("Fizz"), render.TextNode("Buzz"))])).render())
--- render.py
+++ render.py
@@ -4,89 +4,89 @@ import html
 from epyc import render
 
 def sanitise(string):
-	return html.escape(str(string))
+    return html.escape(str(string))
 
 class ParseError(Exception):
-	pass
+    pass
 
 # Node Classes
 # Used in the parsing and evaluation to define the syntax tree.
 class Node:
-	"Node meta-class."
-	def __init__(self, children, content):
-		self.children = children
-		self.content = content
+    "Node meta-class."
+    def __init__(self, children, content):
+        self.children = children
+        self.content = content
 
-	def render(self):
-		raise NotImplementedError("node meta-class cannot be evaluated")
+    def render(self):
+        raise NotImplementedError("node meta-class cannot be evaluated")
 
 
 class GroupNode(Node):
-	def __init__(self, children):
-		super().__init__(children, None)
+    def __init__(self, children):
+        super().__init__(children, None)
 
-	def render(self, scope={}):
-		"Render all children in group."
-		ret = ""
+    def render(self, scope={}):
+        "Render all children in group."
+        ret = ""
 
-		for child in self.children:
-			render = child.render(scope)
-			if render is None:
-				render = ""
-			ret += str(render)
-		return ret
+        for child in self.children:
+            render = child.render(scope)
+            if render is None:
+                render = ""
+            ret += str(render)
+        return ret
 
 
 class TextNode(Node):
-	def __init__(self, content):
-		super().__init__([], content)
+    def __init__(self, content):
+        super().__init__([], content)
 
-	def render(self, scope={}):
-		"Render sanitised content"
-		return self.content
+    def render(self, scope={}):
+        "Render sanitised content"
+        return self.content
 
 
 class IncludeNode(Node):
-	def __init__(self, path):
-		self.path = path
+    def __init__(self, path):
+        self.path = path
 
-	def render(self, scope={}):
-		"Return rendered content from file at path"
-		return render(self.path, scope)
+    def render(self, scope={}):
+        "Return rendered content from file at path"
+        return render(self.path, scope)
 
 class ForNode(Node):
-	def __init__(self, identifier, expression, block):
-		self.identifier = identifier
-		self.expression = expression
-		self.block = block
+    def __init__(self, identifier, expression, block):
+        self.identifier = identifier
+        self.expression = expression
+        self.block = block
 
-	def render(self, scope={}):
-		ret = ''
+    def render(self, scope={}):
+        ret = ''
 
-		for item in eval(self.expression, {}, scope):
-			scope[self.identifier] = item
-			ret += self.block.render(scope) or ''
+        for item in eval(self.expression, {}, scope):
+            scope[self.identifier] = item
+            ret += self.block.render(scope) or ''
 
-		return ret
+        return ret
 
 
 class ExprNode(Node):
-	def __init__(self, content):
-		super().__init__([], content)
+    def __init__(self, content):
+        super().__init__([], content)
 
-	def render(self, scope={}):
-		"Return evaluated content or None"
-		return sanitise(eval(self.content, {}, scope))
+    def render(self, scope={}):
+        "Return evaluated content or None"
+        return sanitise(eval(self.content, {}, scope))
 
 
 class IfNode(Node):
-	def __init__(self, condition, ifnode, elsenode = None):
-		self.ifnode = ifnode
-		self.elsenode = elsenode
-		self.condition = condition
+    def __init__(self, condition, ifnode, elsenode = None):
+        self.ifnode = ifnode
+        self.elsenode = elsenode
+        self.condition = condition
 
-	def render(self, scope={}):
-		if eval(self.condition, {}, scope):
-			return self.ifnode.render(scope)
-		elif self.elsenode:
-			return self.elsenode.render(scope)
+    def render(self, scope={}):
+        if eval(self.condition, {}, scope):
+            return self.ifnode.render(scope)
+        elif self.elsenode:
+            return self.elsenode.render(scope)
