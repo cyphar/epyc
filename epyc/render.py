d2d52d79be1aa0428875dd96fdba7acc5bfdae0f
cyphar <cyphar@cyphar.com>
2014-01-18 00:22:07 +1100

    *: indentation to tabs

--- epyc/render.py
+++ epyc/render.py
@@ -4,108 +4,108 @@ import html
 import epyc
 
 def sanitise(string):
-    return html.escape(str(string))
+	return html.escape(str(string))
 
 class ParseError(Exception):
-    pass
+	pass
 
 # Node Classes
 # Used in the parsing and evaluation to define the syntax tree.
 class Node:
-    "Node meta-class."
-    def __init__(self):
-        pass
+	"Node meta-class."
+	def __init__(self):
+		pass
 
-    def render(self, scope={}, path="."):
-        raise NotImplementedError("node meta-class cannot be evaluated")
+	def render(self, scope={}, path="."):
+		raise NotImplementedError("node meta-class cannot be evaluated")
 
 
 class GroupNode(Node):
-    def __init__(self, children):
-        self.children = children
+	def __init__(self, children):
+		self.children = children
 
-    def render(self, scope={}, path="."):
-        "Render all children in group."
-        ret = ""
+	def render(self, scope={}, path="."):
+		"Render all children in group."
+		ret = ""
 
-        for child in self.children:
-            render = child.render(scope, path)
-            if render is None:
-                render = ""
-            ret += str(render)
-        return ret
+		for child in self.children:
+			render = child.render(scope, path)
+			if render is None:
+				render = ""
+			ret += str(render)
+		return ret
 
 
 class TextNode(Node):
-    def __init__(self, content):
-        self.content = content
+	def __init__(self, content):
+		self.content = content
 
-    def render(self, scope={}, path="."):
-        "Render sanitised content"
-        return self.content
+	def render(self, scope={}, path="."):
+		"Render sanitised content"
+		return self.content
 
 
 class IncludeNode(Node):
-    def __init__(self, path):
-        self.path = path
+	def __init__(self, path):
+		self.path = path
 
-    def render(self, scope={}, path="."):
-        "Return rendered content from file at path"
-        return epyc.render(path + "/" + self.path, scope)
+	def render(self, scope={}, path="."):
+		"Return rendered content from file at path"
+		return epyc.render(path + "/" + self.path, scope)
 
 class ForNode(Node):
-    def __init__(self, identifier, expression, block):
-        self.identifier = identifier
-        self.expression = expression
-        self.block = block
+	def __init__(self, identifier, expression, block):
+		self.identifier = identifier
+		self.expression = expression
+		self.block = block
 
-    def render(self, scope={}, path="."):
-        ret = ''
+	def render(self, scope={}, path="."):
+		ret = ''
 
-        try:
-            L = eval(self.expression, {}, scope)
-        except:
-            return None
+		try:
+			L = eval(self.expression, {}, scope)
+		except:
+			return None
 
-        for item in L:
-            try:
-                getnext = "%s = %r" % (self.identifier, item)
-                exec(getnext, {}, scope)
-            except:
-                return None
+		for item in L:
+			try:
+				getnext = "%s = %r" % (self.identifier, item)
+				exec(getnext, {}, scope)
+			except:
+				return None
 
-            ret += self.block.render(scope, path) or ''
+			ret += self.block.render(scope, path) or ''
 
-        return ret
+		return ret
 
 
 class ExprNode(Node):
-    def __init__(self, content):
-        self.content = content
+	def __init__(self, content):
+		self.content = content
 
-    def render(self, scope={}, path="."):
-        "Return evaluated content or None"
-        try:
-            val = eval(self.content, {}, scope)
-        except:
-            return None
+	def render(self, scope={}, path="."):
+		"Return evaluated content or None"
+		try:
+			val = eval(self.content, {}, scope)
+		except:
+			return None
 
-        return sanitise(val)
+		return sanitise(val)
 
 
 class IfNode(Node):
-    def __init__(self, condition, ifnode, elsenode=None):
-        self.ifnode = ifnode
-        self.elsenode = elsenode
-        self.condition = condition
+	def __init__(self, condition, ifnode, elsenode=None):
+		self.ifnode = ifnode
+		self.elsenode = elsenode
+		self.condition = condition
 
-    def render(self, scope={}, path="."):
-        try:
-            cond = eval(self.condition, {}, scope)
-        except:
-            return self.elsenode.render(scope, path)
+	def render(self, scope={}, path="."):
+		try:
+			cond = eval(self.condition, {}, scope)
+		except:
+			return self.elsenode.render(scope, path)
 
-        if cond:
-            return self.ifnode.render(scope, path)
-        elif self.elsenode:
-            return self.elsenode.render(scope, path)
+		if cond:
+			return self.ifnode.render(scope, path)
+		elif self.elsenode:
+			return self.elsenode.render(scope, path)
--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import sys
 import epyc
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print(epyc.render(sys.argv[1]))
-    else:
-        string = "\n".join(iter(input, ""))
-        print(epyc._render(string, {"a": [1, 2, 3]}))
+	if len(sys.argv) > 1:
+		print(epyc.render(sys.argv[1]))
+	else:
+		string = "\n".join(iter(input, ""))
+		print(epyc._render(string, {"a": [1, 2, 3]}))
 
 # Node Testing
-    #import render
-    #print(render.ForNode("a", "[1, 2, 3]", render.GroupNode([render.TextNode("hello, a:"), render.ExprNode("a"), render.TextNode("!\n")])).render({}))
-    #print(render.IfNode("x > 10", render.TextNode("Statement is TRUE"), render.TextNode("Statement is FALSE")).render({'x':5}))
-    #print(render.ForNode("a", "range(100)", render.GroupNode([render.IfNode("a % 3 == 0", render.TextNode("Fizz"), render.TextNode("Buzz"))])).render())
+	#import render
+	#print(render.ForNode("a", "[1, 2, 3]", render.GroupNode([render.TextNode("hello, a:"), render.ExprNode("a"), render.TextNode("!\n")])).render({}))
+	#print(render.IfNode("x > 10", render.TextNode("Statement is TRUE"), render.TextNode("Statement is FALSE")).render({'x':5}))
+	#print(render.ForNode("a", "range(100)", render.GroupNode([render.IfNode("a % 3 == 0", render.TextNode("Fizz"), render.TextNode("Buzz"))])).render())
